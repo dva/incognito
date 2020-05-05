125edf4f118943eb4d4e69fb299c8923391c7031
dva <denisva@gmail.com>
2020-05-05 23:52:20 +0300

    feat: actual implementation

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log('init')
+const msg = 'Incognito'
+
+async function init () {
+  if ('storage' in navigator && 'estimate' in navigator.storage) {
+    const { usage, quota } = await navigator.storage.estimate()
+    console.log(`Using ${usage} out of ${quota} bytes.`)
+
+    if (quota < 120000000) {
+      document.getElementById('root').textContent = msg
+    } else {
+      document.getElementById('root').textContent = `Not ${msg}`
+    }
+  } else {
+    document.getElementById('root').textContent = '🤷'
+  }
+}
+
+init()
